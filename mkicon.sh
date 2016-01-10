0b3db1488b39a9645b9e8f5e01bd2029149b2906
user <email@example.com>
2016-01-10 12:21:06 +0100

    first commit

--- mkicon.sh
+++
@@ -1,6 +0,0 @@
-convert $1 -resize 192x192 ./app/src/main/res/mipmap-xxxhdpi/ic_launcher.png
-convert $1 -resize 144x144 ./app/src/main/res/mipmap-xxhdpi/ic_launcher.png
-convert $1 -resize 96x96 ./app/src/main/res/mipmap-xhdpi/ic_launcher.png
-convert $1 -resize 72x72 ./app/src/main/res/mipmap-hdpi/ic_launcher.png
-convert $1 -resize 48x48 ./app/src/main/res/mipmap-mdpi/ic_launcher.png
-convert $1 -resize 36x36 ./app/src/main/res/mipmap-ldpi/ic_launcher.png
