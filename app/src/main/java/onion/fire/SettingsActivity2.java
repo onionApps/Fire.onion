2fe8312f91a4db83c60deb939455ee7f54b3785a
user <email@example.com>
2016-02-19 11:51:34 +0100

    -

--- app/src/main/java/onion/fire/SettingsActivity2.java
+++ app/src/main/java/onion/fire/SettingsActivity2.java
@@ -1,3 +1,13 @@
+/*
+ * Fire.onion
+ *
+ * https://play.google.com/store/apps/details?id=onion.fire
+ * http://onionapps.github.io/Fire.onion/
+ * http://github.com/onionApps/Fire.onion
+ *
+ * Author: http://github.com/onionApps - http://jkrnk73uid7p5thz.onion - bitcoin:1kGXfWx8PHZEVriCNkbP5hzD15HS4AyKf
+ */
+
 package onion.fire;
 
 import android.app.AlertDialog;
